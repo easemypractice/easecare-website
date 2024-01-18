97a9f2797022a5ca5c48f7fda3c71c544a8b7f59
Pramesh8006 <pramesh.prasad@ailoitte.com>
2024-01-18 18:13:49 +0530

    url share image preview

--- src/component/Head/head.js
+++ src/component/Head/head.js
@@ -9,6 +9,9 @@ const HeadPart = ({ title, description, imageUrl }) => {
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={imageUrl} />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="600" />
+      <meta property="og:image:type" content="image/jpg" />
     </Head>
   );
 };
