c157f0add003c53f5e2a75a22455f0b08dc1250d
Pramesh8006 <pramesh.prasad@ailoitte.com>
2024-01-24 17:24:00 +0530

    home preivew image

--- src/app/layout.js
+++ src/app/layout.js
@@ -33,10 +33,10 @@ export default function RootLayout({ children }) {
             "Easecare Clarity streamline appointments, patients' health records, billing, and care tracking and is built for high-performance modern healthcare teams."
           }
         />
-        <meta property="og:image" content={"/home-preview.jpg"} />
+        <meta property="og:image" content="/images/home-preview.jpg" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="600" />
-        <meta property="og:image:type" content="image/jpg" />
+        <meta property="og:image:type" content="image/jpeg" />
       </head>
       <body className={"body"}>
         {/* heelo
