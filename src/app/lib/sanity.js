1407583e3c68fbe37bfb20d325cdfd3f2db967b0
AmanAiloitte <144457584+AmanAiloitte@users.noreply.github.com>
2024-05-07 13:08:04 +0530

    removed

--- src/app/lib/sanity.js
+++ src/app/lib/sanity.js
@@ -1,11 +1,12 @@
 import { createClient } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
-import {apiVersion, dataset, projectId} from '../../../sanity/env'
+import {apiVersion, dataset, projectId, readToken} from '../../../sanity/env'
 export const client = createClient({
   apiVersion: apiVersion,
   dataset: dataset,
   projectId: projectId,
   useCdn: false,
+  token:readToken
 });
 const builder = imageUrlBuilder(client);
 export function urlFor(source) {
