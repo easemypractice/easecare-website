f9c72533171fea25d698cb2c3b4be642f926e737
AmanAiloitte <144457584+AmanAiloitte@users.noreply.github.com>
2024-05-07 15:29:23 +0530

    chnage in configration

--- sanity.config.js
+++ sanity.config.js
@@ -7,19 +7,19 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import {apiVersion, dataset, projectId} from './sanity/env'
+// import {apiVersion} from './sanity/env'
 import {schema} from './sanity/schemas/schema'
 
 export default defineConfig({
   basePath: '/studio',
-  projectId,
-  dataset,
+  projectId:"n6nmro7y",
+  dataset:"production",
   // Add and edit the content schema in the './sanity/schema' folder
   schema,
   plugins: [
     structureTool(),
     // Vision is a tool that lets you query your content with GROQ in the studio
     // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({defaultApiVersion: apiVersion}),
+    visionTool({defaultApiVersion: "2024-05-06"}),
   ],
 })
