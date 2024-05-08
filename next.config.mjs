6a4e0001ba15bbd2d6ba6ee6952664a2a6d91ae5
AmanAiloitte <144457584+AmanAiloitte@users.noreply.github.com>
2024-05-08 18:51:51 +0530

    Build error

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "cdn.sanity.io",
+        hostname: "easecare.sanity.studio",
         port: "",
         pathname: "**",
       },
