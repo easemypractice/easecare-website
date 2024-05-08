df58c91d5292756f0bccce268f5a9ff561ca1d68
AmanAiloitte <144457584+AmanAiloitte@users.noreply.github.com>
2024-05-08 18:49:50 +0530

    Build error

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // publicRuntimeConfig: { staticFolder: '/sanity.config.js',},
+  publicRuntimeConfig: { staticFolder: '/sanity.config.js',},
   images: {
     remotePatterns: [
       {
