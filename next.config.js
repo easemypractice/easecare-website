b34f2cb0c48d17961474b4315cb39fa4e509e61c
AmanAiloitte <aman@ailoitte.com>
2024-05-10 13:36:01 +0530

    edited client file

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // publicRuntimeConfig: { staticFolder: '/sanity.config.js',},
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: "",
+        pathname: "**",
+      },
+    ],  
+  },
+};
+
+module.exports = nextConfig;
--- sanity/lib/client.js
+++ sanity/lib/client.js
@@ -3,9 +3,9 @@ import { createClient } from 'next-sanity'
 import { apiVersion, dataset, projectId, useCdn,readToken } from '../env'
 
 export const client = createClient({
-  apiVersion,
-  dataset,
-  projectId,
-  useCdn,
-  readToken
+  projectId:"w2lce9jj",
+  dataset:"production",
+  apiVersion:"2024-05-06",
+  useCdn:false,
+  readToken:"skUtP3B04gPGuJCU9JcVyoavvtzN0aOYAVAZN8ZLHMgVzBPK06wvki2nuoWBOKpXnvWGjNPcyZSzI1NOqdkA9PtnpITdfQ0bHcU9gjCL2Siv6wfGoJKqhhpo1mpt2N6M9vfJER1LQII3IWa355c2prTNXjFIkbpNvb02A1qeUsWEvIS4u1rU"
 })
