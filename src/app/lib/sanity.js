ad2b872ac1681bdb2a3525c8c9ee47b333157399
AmanAiloitte <aman@ailoitte.com>
2024-05-13 10:02:30 +0530

    project id changes

--- src/app/lib/sanity.js
+++ src/app/lib/sanity.js
@@ -1,12 +1,12 @@
 import { createClient } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
-import {apiVersion, dataset, projectId, readToken} from '../../../sanity/env'
+// import {apiVersion, dataset, projectId, readToken} from '../../../sanity/env'
 export const client = createClient({
-  apiVersion: apiVersion,
-  dataset: dataset,
-  projectId: projectId,
+  apiVersion: "2024-05-06",
+  dataset: "production",
+  projectId: "w2lce9jj",
   useCdn: false,
-  token:readToken
+  token:"skUtP3B04gPGuJCU9JcVyoavvtzN0aOYAVAZN8ZLHMgVzBPK06wvki2nuoWBOKpXnvWGjNPcyZSzI1NOqdkA9PtnpITdfQ0bHcU9gjCL2Siv6wfGoJKqhhpo1mpt2N6M9vfJER1LQII3IWa355c2prTNXjFIkbpNvb02A1qeUsWEvIS4u1rU"
 });
 const builder = imageUrlBuilder(client);
 export function urlFor(source) {
