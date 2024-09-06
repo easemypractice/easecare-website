a29c17764ef4382dcf3f7cf54d04c427d671659e
AmanAiloitte <aman@ailoitte.com>
2024-09-06 18:56:11 +0530

    added the active class to table of content blog page onscroll

--- src/pages/blog/[slug].js
+++ src/pages/blog/[slug].js
@@ -63,38 +63,29 @@ const RecentBlogArticle = () => {
     });
   }, [params]);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollPosition = window.scrollY + window.innerHeight;
-      const headings = document.querySelectorAll("h5");
-      // Adjust selectors as needed
-      let newActiveId = "";
-      headings.forEach((heading) => {
-        const rect = heading.getBoundingClientRect();
-        const headingTop = rect.top + window.scrollY;
-        const headingBottom = headingTop + rect.height;
+  // useEffect(() => {
+  const handleScroll = (e) => {
 
-        if (
-          scrollPosition - 100 >= headingTop &&
-          scrollPosition - 50 <= headingBottom
-        ) {
-          newActiveId = heading.id;
-        }
-      });
+    const scrollPosition = window.scrollY + window.innerHeight;
+    const headings = document.querySelectorAll("h5");
+ 
+    let newActiveId = "";
+    headings.forEach((heading) => {
+      const rect = heading.getBoundingClientRect();
+      const headingTop = rect.top + window.scrollY;
+      const headingBottom = headingTop + rect.height;
 
-      console.log(newActiveId);
-      setActiveSection(newActiveId);
-    };
+      if (
+        scrollPosition - 150 >= headingTop &&
+        scrollPosition - 700 <= headingBottom
+      ) {
+        newActiveId = heading.id;
+      }
+    });
+    setActiveSection(newActiveId);
+  };
 
-    // Add event listener for scroll
-    window.addEventListener("scroll", handleScroll);
 
-    // Initial check
-    handleScroll();
-
-    // Cleanup on component unmount
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
 
   useEffect(() => {
     if (activeSection) {
@@ -115,80 +106,85 @@ const RecentBlogArticle = () => {
           description={data?.seo?.metaDescription}
           pageLink={`blog/${params?.slug}`}
         />
-
-        <Box className="container ">
-          <Box className="blog-grp">
-            <Box className="blog-content" id="blogContent1234">
-              <Text as="h1">{data?.title}</Text>
-              <Flex className="author-image">
-                {author && (
+        <div
+          onScroll={handleScroll}
+          style={{ maxHeight: "800px", overflowY: "auto" }}
+        >
+          <Box className="container ">
+            <Box className="blog-grp">
+              <Box className="blog-content" id="blogContent1234">
+                <Text as="h1">{data?.title}</Text>
+                <Flex className="author-image">
+                  {author && (
+                    <Image
+                      src={urlFor(author?.image)?.url() || ""}
+                      width={60}
+                      height={60}
+                      alt="profile-picture"
+                    />
+                  )}
+                  <Flex direction="column" className="author">
+                    <Text as="h4">{author?.title}</Text>
+                    <Text as="span">{formatDate(data?._createdAt)}</Text>
+                  </Flex>
+                </Flex>
+                {data && data?.titleImage && (
                   <Image
-                    src={urlFor(author?.image)?.url() || ""}
-                    width={60}
-                    height={60}
-                    alt="profile-picture"
+                    src={urlFor(data?.titleImage)?.url()}
+                    width={640}
+                    height={320}
+                    style={{ width: "100%" }}
+                    alt="image"
+                    className="content-image"
+                    priority
+                    quality={80}
                   />
                 )}
-                <Flex direction="column" className="author">
-                  <Text as="h4">{author?.title}</Text>
-                  <Text as="span">{formatDate(data?._createdAt)}</Text>
-                </Flex>
-              </Flex>
-              {data && data?.titleImage && (
-                <Image
-                  src={urlFor(data?.titleImage)?.url()}
-                  width={640}
-                  height={320}
-                  style={{ width: "100%" }}
-                  alt="image"
-                  className="content-image"
-                  priority
-                  quality={80}
-                />
-              )}
-              <PostBody content={data?.content} className="blog-content" />
-            </Box>
-            {data && (
-              <Box className="blog-right">
-                <Box
-                  className={`blog-nav ${
-                    data?.currentSlug === "best-physiotherapists-in-dehradun" &&
-                    "pl-5"
-                  }`}
-                >
-                  <h2>Table of Contents</h2>
-                  <ol
-                    className={
-                      data?.currentSlug ===
-                        "best-physiotherapists-in-dehradun" && "list-style-none"
-                    }
-                  >
-                    {pageNav.map((item, index) => (
-                      <Link
-                        className={
-                          activeSection ===
-                          item.text.replace(/ /g, "-").replace(/:/g, "")
-                            ? "active"
-                            : ""
-                        }
-                        key={index}
-                        href={`#${item.text.replace(/ /g, "-").replace(/:/g, "")}`}
-                      >
-                        <li>{item.text.replace(/:/g, "")}</li>
-                      </Link>
-                    ))}
-                  </ol>
-                </Box>
-                <Box className="blog-contact">
-                  <Heading as="h2">Are you looking for EHR Software</Heading>
-                  <Link href={"/book-a-demo"} className="purple-btn">
-                    Book a Free Demo
-                  </Link>
-                </Box>
+                <PostBody content={data?.content} className="blog-content" />
               </Box>
-            )}
+              {data && (
+                <Box className="blog-right">
+                  <Box
+                    className={`blog-nav ${
+                      data?.currentSlug ===
+                        "best-physiotherapists-in-dehradun" && "pl-5"
+                    }`}
+                  >
+                    <h2>Table of Contents</h2>
+                    <ol
+                      className={
+                        data?.currentSlug ===
+                          "best-physiotherapists-in-dehradun" &&
+                        "list-style-none"
+                      }
+                    >
+                      {pageNav.map((item, index) => (
+                        <Link
+                          className={
+                            activeSection ===
+                            item.text.replace(/ /g, "-").replace(/:/g, "")
+                              ? "active"
+                              : ""
+                          }
+                          key={index}
+                          href={`#${item.text.replace(/ /g, "-").replace(/:/g, "")}`}
+                        >
+                          <li>{item.text.replace(/:/g, "")}</li>
+                        </Link>
+                      ))}
+                    </ol>
+                  </Box>
+                  <Box className="blog-contact">
+                    <Heading as="h2">Are you looking for EHR Software</Heading>
+                    <Link href={"/book-a-demo"} className="purple-btn">
+                      Book a Free Demo
+                    </Link>
+                  </Box>
+                </Box>
+              )}
+            </Box>
           </Box>
-        </Box>
+        </div>
       </Layout>
     </div>
   );
